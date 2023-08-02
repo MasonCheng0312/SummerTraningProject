ddfe485586bf519920c7ba5f621149e31322300b
MasonCheng0312 <mason9003122001@gmail.com>
2023-08-02 14:43:54 +0800

    selenium didn't have executable_path in 4.10.0,so we change this parameter to other parameter (service)to make sure our code can be done correctly

--- web_driver.py
+++ web_driver.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
 
 class Web_Driver():
-    def __init__(self, chromedriver_path: str, web_address: str):
+    def __init__(self, chromedriver_path:str, web_address: str):
         self.chrome_options = Options()
         self.chrome_options.add_argument("--mute-audio")
         # 將網頁靜音
-        # self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument("--headless")
         # 將網頁設定在後台運行，不會彈出視窗
         self.chrome_options.add_argument("--disable-gpu")
         # 禁止使用gpu加速，提高爬蟲的穩定性
@@ -17,7 +18,8 @@ class Web_Driver():
         # 在程式結束後不關閉網頁
         self.chrome_options.add_experimental_option("prefs", {"download.default_directory": "/home/cosbi2/py_project/summer_training"})
         # 改變預設下載路徑
-        self.browser = webdriver.Chrome(options=self.chrome_options, executable_path=chromedriver_path)
+        service = Service(executable_path=chromedriver_path)
+        self.browser = webdriver.Chrome(options=self.chrome_options, service=service)
         self.browser.get(web_address)
         time.sleep(4.5)
         for _ in range(2):
@@ -31,4 +33,4 @@ class Web_Driver():
         
     def quit(self):
         self.browser.quit()
-        time.sleep(1)
+        time.sleep(2.5)
