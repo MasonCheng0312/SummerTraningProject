77d5ea863a4135e475aab33bab313bdd4edb1284
MasonCheng0312 <mason9003122001@gmail.com>
2023-08-04 14:49:14 +0800

    basic practice

--- lab_django_training/web_tool/views.py
+++ lab_django_training/web_tool/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from web_tool.models import Gene
+import pandas as pd
+import json
 
 def hello_world(request):  # 看似沒有使用到參數request，但刪除則無法正常載入/hello/
     return HttpResponse("hello django!")
@@ -14,3 +17,26 @@ def get_time(request):
     time = datetime.now()  # 模板中需填入的變數可以透過url傳入，亦可使用function指定
     return render(request, "get_time.html", locals())
     # local()函數代表傳入所有區域變數，此例指time，若在url傳入參數亦是由local打包，如未傳入參數可省略
+
+
+def index(request):
+    df = pd.read_csv("/home/cosbi2/py_project/summer_training/lab_django_training/data/hw1_output.csv")
+    df = df.head(100)
+    df = df.rename(columns={"Gene_ID":"id", "transcript_ID": "transcript", "# of transcripts": "number"})
+
+    json_string = df.to_json(orient='records')
+
+    genes = json.loads(json_string)
+    # 將json資料格式轉成python認識的資料結構，此處為字典
+
+    return render(request, "index.html", locals())
+
+
+def printer(request):
+    test_objects = Gene.objects.filter(gene_id__contains = "WBGene0000001")
+    return render(request, "test.html", locals())
+
+
+
+
+
