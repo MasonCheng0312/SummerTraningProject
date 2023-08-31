114fd15a3bb906b8935346e345ec154e2bc4eb92
MasonCheng0312 <mason9003122001@gmail.com>
2023-08-31 12:47:29 +0800

    integrate python data parser to django version0.5

--- cosbi_summerFinal/static/js/result.js
+++ cosbi_summerFinal/static/js/result.js
@@ -1,5 +1,34 @@
-window.onload = function() {
+function callViewsForTransData(){
+    var currentUrl = window.location.href;
+    var targetUrl = currentUrl.replace("/detail_data/","/getData/")
+    return fetch(targetUrl)        
+};
+
+
+function loadData(Data){//Data為一json檔
     
+
+}
+
+
+
+window.onload = function() {
+    callViewsForTransData()
+        .then(Response => {
+            if(!Response.ok){
+                throw new Error("Response was not ok!");
+            }
+            return Response.json();
+        })
+        .then(Data =>{
+
+        })
+        .catch(error => {
+            console.error("There was some error in fetch operation :", error);
+            throw error;
+        });
 
 
 };
+
+
--- cosbi_summerFinal/web_tool/urls.py
+++ cosbi_summerFinal/web_tool/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from . import views, dataParser
+
 
 urlpatterns = [
     path('ajax_data/', views.ajax_data),
     path('home/',views.form),
     path('detail_data/', views.result),
+    path('getData/', )
 ]
